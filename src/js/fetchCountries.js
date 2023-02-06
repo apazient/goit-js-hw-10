9875cc594043a2aaf8d69e205bd536c012847933
apazient <nataliia.amanzhayeva@web.de>
2023-02-06 09:18:21 +0100

    fixed Error message

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,12 @@
-const BASE_URL = `https://restcountries.com/v3.1/name`
+// import { Notify } from 'notiflix';
+const BASE_URL = `https://restcountries.com/v3.1/name`;
 
-export const fetchCountries =(name) =>{
-    return fetch(`${BASE_URL}/${name}?field=name`).then(r => r.json());
- }
+export const fetchCountries = name => {
+  return fetch(`${BASE_URL}/${name}`).then(r => {
+    if (r.ok) {
+      return r.json();
+      }
+      throw new Error((error));
+    
+  });
+};
